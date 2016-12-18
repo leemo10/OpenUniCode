093a1375a8f4e454cea9034ec19aa179396a1431
leemo10 <leemo10@outlook.com>
2016-12-18 19:34:37 +0000

    updates

--- javaScriptFuncLib.js
+++ javaScriptFuncLib.js
@@ -2,6 +2,42 @@
  //issues alert if the value is blank.
  //code obtained from Open University Module TT284-16J Block 2 Part 3 The Client Side.
 
+ var errorString = "";
+ var validationErrorCount = 0;
+
+ function validateFormSubmission(){
+  var ids = document.querySelectorAll('[id]');
+    Array.prototype.forEach.call(isNotEmptyValue(value, name))
+
+    var isEmpty getElementById('elementId').value;
+        this.isNotEmptyValue(isEmpty, )
+
+  }
+
+
+
+  //get all inputs on screen, add to array and loop through
+  //array to ensure that the values are not null
+  //more id's can be added without needing to change the validaton code
+  function checkForNullInputs(){
+    var formInputs = document.getElementsByTagName('input');
+    for(var i=0; i <= formInputs.length; i++){
+      console.log("there are a total of "+ formInputs.length + " inputs")
+      if(formInputs[i].getAttribute("type") ==="text" && formInputs[i].value ===""){
+        console.log(formInputs[i].getAttribute("name")+ " is a text input");
+          validationErrorCount ++;
+          errorString = errorString + "Value " +formInputs[i].getAttribute("name") + " is blank and should be completed\n";
+      }
+    }
+
+
+  }
+
+  function addToErrorString(var errorStringValue){
+    errorString = errorString + errorStringValue + "\n";
+  }
+
+
  function isNotEmptyValue(value, name)
   {
     if (value == null || value == "")
